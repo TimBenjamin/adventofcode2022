3ce90630a2be59d3eea404a257c8e3467fb27c08
Tim Benjamin <tim@tienjin.com>
2022-12-01 10:30:42 +0000

    day 1 done

--- day_1/day_1.go
+++ day_1/day_1.go
@@ -2,17 +2,47 @@ package day_1
 
 import (
 	"adventofcode2022/util"
+	"sort"
 	"strconv"
 )
 
 var input []string
 
 func partOne() int {
-	return 0
+	mostCalories := 0
+	currentCalories := 0
+	for _, line := range input {
+		if line == "" {
+			if currentCalories > mostCalories {
+				mostCalories = currentCalories
+			}
+			currentCalories = 0
+		} else {
+			c, _ := strconv.Atoi(line)
+			currentCalories += c
+		}
+	}
+	if currentCalories > mostCalories {
+		mostCalories = currentCalories
+	}
+	return mostCalories
 }
 
 func partTwo() int {
-	return 0
+	caloryCounts := []int{}
+	currentCalories := 0
+	for _, line := range input {
+		if line == "" {
+			caloryCounts = append(caloryCounts, currentCalories)
+			currentCalories = 0
+		} else {
+			c, _ := strconv.Atoi(line)
+			currentCalories += c
+		}
+	}
+	caloryCounts = append(caloryCounts, currentCalories)
+	sort.Ints(caloryCounts)
+	return caloryCounts[len(caloryCounts)-3] + caloryCounts[len(caloryCounts)-2] + caloryCounts[len(caloryCounts)-1]
 }
 
 func Call(part string, inputFile string) string {
